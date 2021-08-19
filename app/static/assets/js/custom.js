7e37ac1e8577090c642be0bb922ec3a7109789af
lazyperosn JKing <lazy2000person@gmail.com>
2021-08-19 17:28:24 +0800

    菜单栏优化

--- app/static/assets/js/custom.js
+++ app/static/assets/js/custom.js
@@ -1,5 +1,6 @@
  jQuery(function($) {
-        
+
+
         $(".sidebar-dropdown > a").click(function(){
 	        $(".sidebar-submenu").slideUp(250);
         	if ($(this).parent().hasClass("active")){
@@ -12,9 +13,22 @@
         	}
 
         });
+            $(".page-wrapper").click(function (e) {
+                var filterList = [
+                    $('#toggle-sidebar .fa')
+                ]
+                var isMenuWrapper = !!$('#sidebar').find($(e.target)).length
 
+debugger
+                if(!isMenuWrapper && !filterList.find(item => item.get(0) === e.target) && $('.page-wrapper').attr("class").split(' ').includes('toggled')) {
+                     $(".page-wrapper").toggleClass("toggled");
+                }
+
+            })
          $("#toggle-sidebar").click(function(){
-	         $(".page-wrapper").toggleClass("toggled");	    
+
+	         $(".page-wrapper").toggleClass("toggled");
+
        	 });
 
            if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
